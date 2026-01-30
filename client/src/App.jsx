d72579240234a42e740f5a48a5b29315b6c96abd
jjacque1 <jacksonjacque.jj@gmail.com>
2026-01-30 17:01:30 -0500

    Add basic routing and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,26 @@
-export default function App() {
-  return <div>App</div>;
+import { Route, Routes, Link } from "react-router-dom"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
+
+function App() {
+  return (
+    <div>
+      <h1>Task Manger</h1>
+
+      <nav>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign up</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+      </Routes>
+    </div>
+  )
 }
+
+export default App;

66b94f03bd697d4527e943dde6391ce95a451ae0
jjacque1 <jacksonjacque.jj@gmail.com>
2026-01-30 10:27:34 -0500

    Added projects dashboard route, passed test in postman

--- server/src/routes/projectRoutes.js
+++ server/src/routes/projectRoutes.js
@@ -24,4 +24,16 @@ router.post("/", authMiddleware, async (req, res) => {
   }
 });
 
+// GET ALL PROJECTS 
+
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const projects = await Project.find({ owner: req.user._id }).sort({ createdAt: -1 });
+    return res.status(200).json(projects);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+
 module.exports = router;

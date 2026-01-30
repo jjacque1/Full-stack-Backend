700953d3ad873e617a1d823f864c486f1213c504
jjacque1 <jacksonjacque.jj@gmail.com>
2026-01-29 23:55:35 -0500

    Combine app and server into single server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,16 @@
 require("dotenv").config();
 require("./src/config/db");
 
-console.log("Server started");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Task App API is alive and well");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});

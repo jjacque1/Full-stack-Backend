3d446366b1544ef423563907e571f1f46e1e0778
jjacque1 <jacksonjacque.jj@gmail.com>
2026-01-30 09:46:06 -0500

    project routes created, moving routes in appropriate files to clean up server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const authMiddleware = require("./src/middleware/authMiddleware");
+const Project = require("./src/models/Projects");
 
 
 app.use(cors());
@@ -117,6 +118,28 @@ app.post("/api/auth/login", async (req, res) => {
         email: user.email,
       },
     });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+//=======Project route================//
+
+app.post("/api/projects", authMiddleware, async (req, res) => {
+  try {
+    const { name, description } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ message: "Please enter a project name to continue" });
+    }
+
+    const project = await Project.create({
+      owner: req.user._id,
+      name,
+      description,
+    });
+
+    return res.status(201).json(project);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }

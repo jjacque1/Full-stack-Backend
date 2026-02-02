98c6f7a6fa51f18d2ee2012dd26b9d111105dab0
jjacque1 <jacksonjacque.jj@gmail.com>
2026-02-02 11:16:04 -0500

    chore(App): remove redundant /dashboard/projects route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
-import Projects from "./components/Projects";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ProjectPage from "./pages/ProjectPage";
@@ -18,7 +17,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/projects" element={<Projects />} />
         <Route path="/projects/:projectId" element={<ProjectPage />} />
 
         <Route path="/" element={<Home />} />
